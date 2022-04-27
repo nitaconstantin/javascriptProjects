693a635cbb32c14ae7e9032d4ed192c87ce608e6
Constantin Nita <nita.constantin880722@yahoo.com>
2022-04-27 19:09:47 +0300

    navbar revies video projects

---
+++ reviews2.0/reviews2.0.js
@@ -0,0 +1,91 @@
+// local reviews data
+const reviews = [
+    {
+      id: 1,
+      name: "susan smith",
+      job: "web developer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      text:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: "anna johnson",
+      job: "web designer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      text:
+        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    },
+    {
+      id: 3,
+      name: "peter jones",
+      job: "intern",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      text:
+        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    },
+    {
+      id: 4,
+      name: "bill anderson",
+      job: "the boss",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      text:
+        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+  ];
+
+var img = document.getElementById('person-img');
+var author = document.getElementById('author');
+var job = document.getElementById('job');
+var info = document.getElementById('info');
+
+var currentItem = 0;
+
+// create buttons
+var btnPrev = document.querySelector('.prev-btn');
+var btnNext = document.querySelector('.next-btn');
+var btnRandom = document.querySelector('.random-btn');
+
+btnPrev.addEventListener('click', function(){
+    currentItem--;
+    if(currentItem < 0){
+        currentItem = reviews.length - 1;
+    }
+    showPerson();
+})
+
+btnNext.addEventListener('click', function(){
+    currentItem++;
+    if(currentItem > reviews.length - 1){
+        currentItem = 0;
+    }
+    showPerson();
+})
+
+btnRandom.addEventListener('click', function(){
+    var randomNumber = currentItem = Math.floor(Math.random() * reviews.length)
+    showPerson(randomNumber);
+})
+
+
+
+
+
+function showPerson(){
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+    console.log(currentItem);
+
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    showPerson();
+})
+
